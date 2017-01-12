531343f359c4eb0654b15418bade92ce5c94c6b8
ThomasHSmith <thomashoracesmith@gmail.com>
2017-01-11 22:28:12 -0800

    z-cutoff working

--- CorrelationAnalysis.py
+++ CorrelationAnalysis.py
@@ -45,79 +45,43 @@ def main(argv):
 	df = pd.read_pickle(PICKLE_INFILE)
 	log_echo('Loaded dataframe from file: %s' % PICKLE_INFILE.split('/')[-1])
 
-	vals_dict = {'11':-2, '01':0, '02':1, '06':2}
-	df['TissueType'] = df['SampleType'].apply(lambda x: vals_dict[x])
 
-	total_n = df.shape[0]
-	norm_n = df[df['TissueType'] == -2].shape[0]
-	solid_n = df[df['TissueType'] == 0].shape[0]
-	mets_n = df[df['TissueType'] == 2].shape[0]
-	recurr_n = df[df['TissueType'] == 1].shape[0]
+	""" Sample Info """	
+	# Output sample types (ie Normal tissue, solid tumor, metastatic)
+	msg = ''
+	for item in df.SampleType.unique():
+	    msg+='%s: %d' % (item, sum(df.SampleType==item))
+	msg+='Total: %d' % len(df.SampleType)
+	log_echo(msg)
 
-
-	log_echo('\n\tTotal cases: %i\n\tSolid Tumor:%i\n\tMetastatic:%i\n\tRecurrent Solid Tumor:%i\n\tNormal Tissue:%i\n' % (total_n, solid_n, mets_n, recurr_n, norm_n))
-
-	
-	## FIXME ADD OPTION TO DROP SUBTYPES
-
+	## FIXME Add opt to drop subtypes before pearsons calc
 	#pre_drop = df.shape[0]
 	#df = df[df['TissueType'] != -3]
 	#log_msg = 'Dropped %i metastatic cases' % (pre_drop- df.shape[0])
 	#log_echo(log_msg)
-
+	
 	## FIXME Add option to include SampleType in correlation calculations?
 	# Grab all non-categorical data
-	df_vals = df.drop(['PtID','TumorStage','SampleType'],axis=1)
-	
-	## FIXME Add option for zero-filtering
-#	L = []
-#	zero_cutoff = 50
-#	for column in df_vals:
-#    		if (df_vals[df_vals[column]== 0].shape[0]) > zero_cutoff:
-#        		L.append(column)	
-#	df_vals.drop(L, inplace=True, axis=1)
-#	log_echo('Dropped %d targets from list with zero values for > %d samples' % (len(L), zero_cutoff))
-	
-	z_cutoff = 5
-	log_echo('Filtering out cases with z-score > %i' % z_cutoff)
-	pre_filtered_n = len(df_zscores)
-	log_echo('Number of samples (before Z-cutoff): %d' % (pre_filtered_n))
-	df_zscores = pd.DataFrame()
-	for column in df_vals:
-   		df_zscores[column] = stats.zscore(df_vals[column])
-	
-	df_zscores = pd.DataFrame()
-	for column in df:
-    		df_zscores[column] = stats.zscore(df[column])
-	df_zscores['TissueType'] = df['TissueType'].values
-	for column in df_zscores:
-    		df_zscores.drop(df_zscores[df_zscores[column] > z_cutoff_high].index, inplace=True)
-	
-	post_filtered_n = len(df_zscores)
-	pct_lost = ( (pre_filtered_n - post_filtered_n)/float(pre_filtered_n)) * 100
-	log_echo('Number of samples (after Z-cutoff): %d' % (post_filtered_n))
-	log_echo('Dropped %d samples (%f%% of total dataset)\n' % ((-1*(post_filtered_n - pre_filtered_n)), pct_lost))
-	
 
-	# Move target gene to first position
-	target_col = df_zscores[TARGET_GENE]
-	df_zscores.drop(labels=[TARGET_GENE], axis=1, inplace=True)
-	df_zscores.insert(0, TARGET_GENE, target_col)
 
-	## FIXME Add option to inverse sorting
-	df_zscores.sort_values(TARGET_GENE, ascending=False, inplace=True)
-
+	""" Correlation calculations """
 	# Compute pearson and spearman correlation coefficients for target gene and all target genes
 	# Write results to tab-delimited txt file
-	log_echo('Computing correlation coefficients...')
 
+	df_corr = df.select_dtypes(exclude=['object'])
+	target_col = df_corr[TARGET_GENE]
+	df_corr.drop(labels=[TARGET_GENE], axis=1, inplace=True)
+	df_corr.insert(0, TARGET_GENE, target_col)
+
+	## FIXME Make this a function, then add opt to choose whether to calc correlations	
+	log_echo('Computing correlation coefficients...')
 	f = open(CORRELATION_OUTFILE, 'w')
-	x = list(df_zscores[TARGET_GENE])
+	x = list(df_corr[TARGET_GENE])
 	header = 'Gene\t PearsonR (p)\t SpearmanR (p)'
 	log_echo(header)
 	f.write(header + '\n')
-	for column in df_zscores:
-	    y = list(df_zscores[column])
+	for column in df_corr:
+	    y = list(df_corr[column])
 	    pearsonR, pearsonP = stats.pearsonr(x, y)
 	    spearmanR, spearmanP = stats.spearmanr(x, y)
 	    line = '%s\t%.3f (%.3E)\t%.3f (%.3E)' % (column, pearsonR, pearsonP, spearmanR, spearmanP)
@@ -126,16 +90,30 @@ def main(argv):
 	f.close()
 	log_echo('Saved correlation data to: %s\n' % CORRELATION_OUTFILE.split('/')[-1])
 
-## FIXME GET Z_SCORE CUTOFF FROM COMMAND LINE
-## FIXME APPLY Z-CUTOFF BEFORE CALC CORRELATION COEFF
-	df_filtered = pd.DataFrame()
-	for column in df_zscores:
-		df_filtered[column] = df_zscores[df_zscores[column] <= z_cutoff].index
-		#df_zscores.drop(df_zscores[df_zscores[column] > z_cutoff].index, inplace=True)
-	#post_filtered_n = len(df_zscores)
-	## FIXME FIG HEIGHT MIN
-	# Generate heatmap
-
+	""" Calculate z-scores """
+	# Create new df for z-scores
+	z_cutoff = 5 ## FIXME DEFAULT Z CUTOFF
+	df_z = df.select_dtypes(exclude=['object']).apply(stats.zscore)	
+	SampleToInt = {'NormalControl':-3, 'SolidTumor':0, 'Metastatic':0}
+	df_z.insert(len(df_z.columns), 'SampleType', df['SampleType'].apply(lambda x: SampleToInt[x]))
+	target_col = df_z[TARGET_GENE]
+	df_z.drop(labels=[TARGET_GENE], axis=1, inplace=True)
+	df_z.insert(0, TARGET_GENE, target_col)
+	df_z.sort_values(TARGET_GENE, ascending=False, inplace=True)
+	## FIXME Add option to inverse sorting
+	
+	# Filter out samples with z-scores above or below threshold
+	log_echo('Filtering out cases with (z-score > %i) or (z-score < -%i)' % (z_cutoff, z_cutoff))
+	pre_filtered_n = len(df_z)
+	log_echo('Number of samples before z-cutoff: %d' % (pre_filtered_n))
+	df_z = df_z[~(df_z > 5).any(axis=1)]
+	df_z = df_z[~(df_z < -5).any(axis=1)]
+	post_filtered_n = len(df_z)
+	pct_lost = ( (pre_filtered_n - post_filtered_n)/float(pre_filtered_n)) * 100
+	log_echo('Number of samples after z-cutoff: %d' % (post_filtered_n))
+	log_echo('Dropped %d samples (%f%% of total dataset)\n' % ((-1*(post_filtered_n - pre_filtered_n)), pct_lost))
+	
+	""" Generate heatmap """
 	import seaborn as sns
 	import matplotlib.pyplot as plt
 
@@ -143,7 +121,7 @@ def main(argv):
 	if post_filtered_n > 70:
 		FIG_HEIGHT = int(post_filtered_n/45)
 	plt.figure(figsize=(25,FIG_HEIGHT))
-	sns.heatmap(df_zscores,yticklabels=False, xticklabels=True)
+	sns.heatmap(df_z,yticklabels=False, xticklabels=True)
 	#plt.show()
 	plt.savefig(HEATMAP_OUTFILE, bbox_inches = 'tight',dpi=300)
 	log_echo('Saved heatmap as %s' % HEATMAP_OUTFILE.split('/')[-1])
