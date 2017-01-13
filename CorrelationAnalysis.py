5937f8fcf2bfd282a2316ef770b053c073cf8cc0
ThomasHSmith <thomashoracesmith@gmail.com>
2017-01-12 18:35:41 -0800

    added opt to inverse sorting, disable heatmap, choose heatmap output format, disable correlation analysis

--- CorrelationAnalysis.py
+++ CorrelationAnalysis.py
@@ -2,6 +2,9 @@
 # to compare expression of target gene with other genes in input dataset
 
 import sys, argparse, os, datetime, errno
+import pandas as pd
+from scipy import stats
+
 
 #def main(argv):
 
@@ -26,28 +29,34 @@ parser.add_argument('-o', '--output_dir', required=False,
 
 parser.add_argument('-p','--project_name', required=False,
 	help="Short name to add on to the output files "
-	"ie '-p BRCA_F2RL3_20160606'  Default is target/date_time",
+	"Ex: '-p BRCA_F2RL3_20160606'  Default is target/date_time",
 	default=(now[0] + '_' + now[1][:2]+now[1][3:5]))
 		## FIXME Add ability to write several targets
 		## USE add_argument(' ' action='append', dest=targets,default=[])
+
 parser.add_argument('-z','--z_cutoff', required=False,
 	help="Exclude samples with z-scores above and"
 	"below a certain threshold.  Default is 5. Set to 0 "
 	"for no cutoff.  Set to -1 to disable z-score conversion",
 	default=5, type=int)
+
 parser.add_argument('--no_heat', required=False,
 	help="Option to disable generation of heatmap",
 	action='store_true')
+
 parser.add_argument('--no_corr', required=False,
 	help="Option to disable generation of correlation data",
 	action='store_true')
 
+parser.add_argument('--invert', required=False,
+	help="Option to invert sorting of target gene on heatmap "
+	"ie lowest expressers will be on top", default=False,
+	action='store_true')
 
+parser.add_argument('-f','--file_format', required=False,
+	help="Option to save heatmap as different format "
+	"(eps, ps, or pdf) default is pdf", default='pdf')
 
-
-
-import pandas as pd
-from scipy import stats
 args = parser.parse_args()
 
 def make_out_dir(path):
@@ -67,7 +76,7 @@ print '\n\t***************'
 	
 	
 # Build path to input/output files
-HEATMAP_OUTFILE = '%s/%s_%s_heatmap.pdf' % (OUT_PATH, PROJECT_NAME, TARGET_GENE)
+HEATMAP_OUTFILE = '%s/%s_%s_heatmap.%s' % (OUT_PATH, PROJECT_NAME, TARGET_GENE, args.file_format)
 CORRELATION_OUTFILE = '%s/%s_%s_correlations.txt' %  (OUT_PATH, PROJECT_NAME, TARGET_GENE)
 ANAL_LOG_OUTFILE = '%s/%s_%s_analysis_log.txt' %  (OUT_PATH,PROJECT_NAME, TARGET_GENE)
 
@@ -76,8 +85,8 @@ def log_echo(msg):
 	print '\t' + msg
 	lf.write(msg+'\n')
 
-log_echo('Writing analysis log file to: %s' % (ANAL_LOG_OUTFILE.split('/')[-1]) )
-log_echo('TCGA Project: %s' % PROJECT_NAME)
+log_echo('Writing analysis log file to: %s' % (ANAL_LOG_OUTFILE ))
+log_echo('Project name: %s' % PROJECT_NAME)
 log_echo('Gene of interest: %s' % TARGET_GENE)
 df = pd.read_pickle(PICKLE_INFILE)
 log_echo('Loaded dataframe from file: %s\n' % PICKLE_INFILE.split('/')[-1])
@@ -147,8 +156,7 @@ df_z.insert(len(df_z.columns), 'SampleType', df['SampleType'].apply(lambda x: Sa
 target_col = df_z[TARGET_GENE]
 df_z.drop(labels=[TARGET_GENE], axis=1, inplace=True)
 df_z.insert(0, TARGET_GENE, target_col)
-df_z.sort_values(TARGET_GENE, ascending=False, inplace=True)
-	## FIXME Add option to inverse sorting
+df_z.sort_values(TARGET_GENE, ascending=args.invert, inplace=True)
 
 if Z_CUTOFF > 0: # Filter samlpes if a z-score cutoff was provided
 	log_echo('Filtering out cases with (z-score > %i) or (z-score < -%i)' % (Z_CUTOFF, Z_CUTOFF))
